02aa35548f3450235c6d6a228c5bf5520c7e777f
Vaibhav Tandon <you@example.com>
2018-10-19 15:38:02 +0530

    Added broadcast message

--- public/chat.js
+++ public/chat.js
@@ -4,6 +4,7 @@ var msg = document.getElementById('msg');
 var handle = document.getElementById('handler');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var typ = document.getElementById('typing-message');
 
 btn.addEventListener("click", function(){
 	socket.emit('chat', {
@@ -12,7 +13,17 @@ btn.addEventListener("click", function(){
 	});
 });
 
+msg.addEventListener("keypress", function(){
+	socket.emit('typing', handle.value);
+});
+
 socket.on('chat', function(data){
-	if(data.handle!==''&&data.message!=='')
+	if(data.handle!==''&&data.message!==''){
+	typ.innerHTML = "";	
 	output.innerHTML += '<div class="messages"><p><strong>'+data.handle+':</strong>'+data.message+'</p></div>';
+	}
+});
+
+socket.on('typing', function(data){
+	typ.innerHTML = data+" is typing...";
 });
